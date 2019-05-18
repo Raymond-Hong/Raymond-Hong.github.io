a7a12495a0617fd5cfd204f5324d706277468e50
raymond <honxidan@163.com>
2019-05-18 22:38:11 +0800

    deploy

---
+++ js/config/textPath.js
@@ -0,0 +1,20 @@
+export const textPaths = [
+    {
+        path: {
+            path: '/source/technique/js1-0%E4%B8%8Etrue-false%E4%BA%92%E8%BD%AC.html'
+        },
+        title: "js1-0与true-false互转"
+    },
+    { 
+        path: {
+            path:'/source/life/%E7%94%9F%E6%B4%BB.html'
+        }, 
+        title: "生活" 
+    },
+    { 
+        path: {
+            path:'/source/technique/%E7%89%B9%E6%AE%8A%E7%9A%84%E6%95%B0%E5%AD%97.html'
+        }, 
+        title: "特殊的数字" 
+    }
+]
